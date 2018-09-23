1c4c068b481b213eb12fb91161cb360912d77383
César Cárdenas <cesar.cardenas@hundred.com.pe>
2018-09-23 12:33:50 -0500

    Se agrega scripts para recrear base de datos en mongodb

---
+++ scripts/RecrearBaseDatosMongoDB.js
@@ -0,0 +1,15 @@
+var colls = db.getCollectionNames();
+colls.forEach(function(collName) {
+    var coll =db.getCollection(collName);
+    var indexes = coll.getIndexes().splice(1);
+	print("Eliminando colección: " + collName);
+    print(coll.drop());
+	print("Creando colección: " + collName);
+    db.createCollection(collName);
+    var ncoll = db.getCollection(collName);
+    
+    indexes.forEach(function(idx){
+		print("Creando índice: " + idx.name);
+        ncoll.createIndex(idx.key);
+    })
+})
